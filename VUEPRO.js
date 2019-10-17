08701fb7b9154a98048dad40fded3f8d695b1c1c
KimSeunghoo <48181664+KimSeunghoo@users.noreply.github.com>
2019-10-18 03:04:47 +0900

    Update VUEPRO.js

--- VUEPRO.js
+++ VUEPRO.js
@@ -1,9 +1,12 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.isCustomized = true;
-obj.isPremium = true;
-obj.active = true;
+obj.content["isCustomized"] = "true";
+
+obj.content["isPremium"] = "true";
+
+obj.content["active"] = "true";
+
 
 body = JSON.stringify(obj);
 $done(body);
