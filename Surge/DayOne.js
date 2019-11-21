17fea61b6d2ce69d5cf2589f310085673a7bbd38
KimSeunghoo <48181664+KimSeunghoo@users.noreply.github.com>
2019-11-21 10:05:20 +0900

    Create DayOne.js

---
+++ Surge/DayOne.js
@@ -0,0 +1,30 @@
+/*
+http-response ^https:\/\/dayone\.me\/api\/(users|v2\/users\/account-status)$ requires-body=1,max-size=0,script-path=scripts/dayone.js
+
+MitM = dayone.me
+
+*/
+
+if ($response.status == 200) {
+        if ($request.url.endsWith("account-status")) {
+
+            $done({body: JSON.stringify({
+  "expirationDate": 1893427199000,
+  "startDate": 1546272000000,
+  "subscriptionPlan": "com.bloombuilt.dayoneios.subscription.premium.yearly_discounted_trial",
+  "lastRenewalDate": 1546531200000,
+  "subscriptionName": "premium",
+  "bundleReason": "purchase",
+  "cancellationDate": 0
+} )});
+
+        } else {
+	     const bodyJson = JSON.parse($response.body);
+
+	     bodyJson.featureBundle = {"bundleName":"premium","features":[{"name":"imagesPerEntry","limit":30,"canUpgrade":false},{"name":"printingDiscount","canUpgrade":false},{"name":"syncMac","canUpgrade":false},{"name":"prioritySupport","canUpgrade":false},{"name":"sync","canUpgrade":false},{"name":"journalLimit","limit":100,"canUpgrade":false},{"name":"audioPerEntry","limit":10,"canUpgrade":false}]};
+
+	    $done({body: JSON.stringify(bodyJson)});
+        }
+} else {
+	$done({});
+}
